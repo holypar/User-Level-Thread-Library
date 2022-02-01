50ba4a092be8b43aed2514010a9055688b8fff66
pexiao <pexiao@ucdavis.edu>
2022-01-31 20:04:02 -0800

    Queue version 1

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -73,7 +73,7 @@ int queue_enqueue(queue_t queue, void *data)    //
 		queue->length = queue->length + 1; 
 	}
 	
-	return 0;
+	return SUCCESS;
 }
 
 int queue_dequeue(queue_t queue, void **data)
@@ -86,31 +86,59 @@ int queue_dequeue(queue_t queue, void **data)
 	queue->front = nextNode; 
 	nextNode->prev = NULL; 
 	
-	return 0;
+	return SUCCESS;
 }
 
 int queue_delete(queue_t queue, void *data)
 {
+	
+	if (queue == NULL || data == NULL || queue->length == 0) 
+		return -1;
+
 	struct node* currentNode; 
 	currentNode = queue->front; 
-
+	
 	while(currentNode != NULL) {
 		if (currentNode->data == data) {
+			/* Save the next and previous nodes */
 			struct node* prevNode = currentNode->prev; 
 			struct node* nextNode = currentNode->next;  
+			/* Reset the pointers */
 			prevNode->next = nextNode; 
 			nextNode->prev = prevNode; 
-			free(currentNode);
+			/* Free the memory */
+			free(currentNode); 
 		}
+		/* Iterate through the linked list */
 		currentNode = currentNode->next; 
 	}
 	
-	return -1;
+	return SUCCESS;
 }
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
 {
-	return -1;
+
+	if (queue == NULL || func == NULL) 
+		return ERROR; 
+
+	/* Creating node */
+	struct node* currentNode; 
+	currentNode = queue->front;
+
+	while(currentNode != NULL) {
+
+		/* Running the callback function and getting return value */
+		int returnValue = func(queue, currentNode->data, arg);
+
+		/* If return value is 1, we set the data to current node's data and stop iteration */
+		if (returnValue) {
+			*data = currentNode->data; 
+			break; 
+		}
+	}
+	
+	return SUCCESS;
 }
 
 int queue_length(queue_t queue)
