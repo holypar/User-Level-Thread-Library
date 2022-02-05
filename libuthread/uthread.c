6fcc296af91e713917eb591903637e5cb1a6987d
holypar <singhparminder9411@gmail.com>
2022-02-05 13:52:47 -0800

    v0 of threads

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -26,6 +26,7 @@ struct scheduler {
 	queue_t readyQueue; 
 	queue_t blockQueue; 
 	queue_t zombieQueue; 
+	int activeThreadId;
 };
 
 int uthread_start(int preempt)
@@ -39,16 +40,11 @@ int uthread_start(int preempt)
 	scheduler->readyQueue = readyQueue;
 	scheduler->blockQueue = blockQueue;
 	scheduler->zombieQueue = zombieQueue;
-
+	
 	/*Creation of main thread */
 	struct thread* mainThread = malloc(sizeof(struct thread)); 
 	mainThread->tid = 0;
-	uthread_ctx_t* context = malloc(sizeof(uthread_ctx_t));
-	mainThread->stackPointer = uthread_ctx_alloc_stack();
-	uthread_ctx_init(context, mainThread->stackPointer, main()); 
-	mainThread->context = context; 
-	mainThread->state = READY_STATE;
-	queue_enqueue(scheduler->readyQueue, mainThread);
+	scheduler->activeThreadId = mainThread->tid;
 
 	/* TODO */
 	return -1;
@@ -56,14 +52,33 @@ int uthread_start(int preempt)
 
 int uthread_stop(void)
 {
-
+	
 	/* TODO */
 	return -1;
 }
 
 int uthread_create(uthread_func_t func)
 {
-	/* TODO */
+	struct thread* newThread = malloc(sizeof(struct thread));   
+	void* topOfStack = uthread_ctx_alloc_stack();
+	newThread->stackPointer = topOfStack;
+	// if there are no zombie process then we can create a new thread only. https://www.geeksforgeeks.org/zombie-processes-prevention/
+	/* once that if statement is checked you can now create a new thread and make its its threadid = activeThreadId++
+	initilize context 
+	return the id.
+	*/
+	struct scheduler* scheduler = malloc(sizeof( struct scheduler));     //i think i need to make this struct global cause im pretty sure the scheduler is empty here again cause its fresh memory.
+
+	if (queue_length(scheduler->zombieQueue == 0)){
+		newThread->tid = scheduler->activeThreadId++;
+		uthread_ctx_t* context = malloc(sizeof(uthread_ctx_t));
+		uthread_ctx_init(context,newThread->stackPointer,func);	
+		queue_enqueue(scheduler->readyQueue,newThread);  //adds the new created thread to the ready queue
+		return newThread->tid;
+
+	}
+
+
 	return -1;
 }
 
