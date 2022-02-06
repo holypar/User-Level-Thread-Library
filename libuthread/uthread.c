8b0534d9f20a7c2de7b68c6daac7b4b6d5cb0f39
holypar <singhparminder9411@gmail.com>
2022-02-05 17:39:21 -0800

    fixed stuff
    Co-authored-by: pexiao <pexiao@users.noreply.github.com>

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -14,6 +14,8 @@
 #define ACTIVE_STATE 1
 #define BLOCK_STATE 2
 #define ZOMBIE_STATE 3   
+#define SUCCESS 0
+#define ERROR -1 
 
 struct thread {
 	uthread_t tid; 
@@ -22,10 +24,10 @@ struct thread {
 	int state; 
 }; 
 
-
 struct scheduler {
 	int threadCounter; 
 	queue_t readyQueue;  
+	queue_t zombieQueue; 
 	struct thread* activeThread; 
 };
 
@@ -33,17 +35,20 @@ struct scheduler scheduler;
 
 int uthread_start(int preempt)
 {
-	
+	/* Initialize the queue */
 	queue_t readyQueue = queue_create();
-	
+	queue_t zombieQueue = queue_create(); 
+
 	scheduler.readyQueue = readyQueue;
+	scheduler.zombieQueue = zombieQueue; 
 	scheduler.threadCounter = 1; 
+
 	/*Creation of main thread */
 	struct thread* mainThread = malloc(sizeof(struct thread)); 
 	mainThread->tid = scheduler.threadCounter - 1;
 	scheduler.activeThread = mainThread;
 
-	return -1;
+	return SUCCESS;
 }
 
 int uthread_stop(void)
@@ -58,23 +63,33 @@ int uthread_stop(void)
 		/* Free the ready queue */
 		queue_destroy(scheduler.readyQueue); 
 		
-		return 0;
+		return SUCCESS;
 	}
 	
-	/* TODO */
-	return -1;
+	return ERROR;
 }
 
 int uthread_create(uthread_func_t func)
 {
-	/* Creates new thread */
-	struct thread* newThread = malloc(sizeof(struct thread));   
+	/* Creates new thread struct */
+	struct thread* newThread = malloc(sizeof(struct thread));
+	if (newThread == NULL)
+		return ERROR;    
+	
+	/* Creates stack pointer */
 	void* topOfStack = uthread_ctx_alloc_stack();
+	if (topOfStack == NULL)
+		return ERROR; 
 	newThread->stackPointer = topOfStack;
 	scheduler.threadCounter = scheduler.threadCounter + 1; 
 	newThread->tid = scheduler.threadCounter - 1; 
 
+	/* Creates context */
 	uthread_ctx_t* context = malloc(sizeof(uthread_ctx_t));
+	if (context == NULL)
+		return ERROR;
+	
+	/* Initializes contexts and stores it */
 	uthread_ctx_init(context, newThread->stackPointer, func);
 	newThread->context = context; 
 	newThread->state = READY_STATE; 
@@ -84,7 +99,6 @@ int uthread_create(uthread_func_t func)
 	
 	return newThread->tid;
 
-	/* TODO Handle Errors */
 }
 
 void uthread_yield(void)
@@ -92,14 +106,18 @@ void uthread_yield(void)
 	
 	/* If the ready queue is not empty, push the current thread to the back of the queue and resume the next ready thread */
 	if (queue_length(scheduler.readyQueue) != 0) {
-		void* nextThread;
-		queue_dequeue(scheduler.readyQueue, (void**)&nextThread);
+		/* Dequeue the thread from the ready queue as the next thread */
+		struct thread* nextThread;
+		queue_dequeue(scheduler.readyQueue, &nextThread);
+
+		/* Enqueue the current active thread to the back of the queue */
 		queue_enqueue(scheduler.readyQueue, scheduler.activeThread); 
-		uthread_ctx_switch(scheduler.activeThread, nextThread); 
+
+		/* Context switches the context */
+		uthread_ctx_switch(scheduler.activeThread->context, nextThread->context); 
 		scheduler.activeThread = nextThread; 
 	} 
 
-	/* TODO */
 }
 
 uthread_t uthread_self(void)
@@ -109,12 +127,24 @@ uthread_t uthread_self(void)
 
 void uthread_exit(int retval)
 {
-	/* TODO */
+	scheduler.activeThread->state = ZOMBIE_STATE; 
+	//enque the active thread into the zombiequeue
+	//save return value somehow 
+	
 }
 
 int uthread_join(uthread_t tid, int *retval)
 {
 	/* TODO */
+	// Understand how to get the return value 
+	// Decide whether to use a zombie queue or a zombie state 
+ 	while(1)
+	{
+		if (scheduler.activeThread->state == ZOMBIE_STATE) 
+			break; 
+		
+	}
+	
 	return -1;
 }
 
